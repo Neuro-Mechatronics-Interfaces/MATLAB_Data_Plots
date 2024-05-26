faac722e9549dfc9e46398acfe42a4b95cc49966
Max (Lenovo) <max.murphy11@gmail.com>
2024-05-26 14:24:31 -0400

    Adds ability to scale the scale bar in x/y axes

--- add_scale_bar.m
+++ add_scale_bar.m
@@ -12,20 +12,23 @@ arguments
     y1 (1,1) double % Ending point of vertical bar
     options.Color (1,3) double {mustBeInRange(options.Color,0,1)} = [0.65 0.65 0.65];
     options.XUnits = 'sec';
+    options.XLabelScaleFactor = 1;
     options.YUnits = 'μV';
+    options.YLabelScaleFactor = 1;
+    options.FontSize = 12;
 end
 % Add scalebar to plot
-dx = round(abs(x1 - x0));
+dx = round(abs(x1 - x0).*options.XLabelScaleFactor)./options.XLabelScaleFactor;
 x1 = x0 + dx;
-dy = round(abs(y1 - y0));
+dy = round(abs(y1 - y0).*options.YLabelScaleFactor)./options.YLabelScaleFactor;
 y1 = y0 + dy;
 
-tx = sprintf('%d %s', dx, options.XUnits); % time scalebar text
-ty = sprintf('%d %s', dy, options.YUnits); % vertical scalebar text
+tx = sprintf('%d %s', dx * options.XLabelScaleFactor, options.XUnits); % time scalebar text
+ty = sprintf('%d %s', dy * options.YLabelScaleFactor, options.YUnits); % vertical scalebar text
 h.XBar = line(ax, [x0, x0], [y0, y1], 'Color',options.Color,'LineWidth',1.25,'LineStyle','-');
 h.YBar = line(ax, [x0, x1], [y0, y0], 'Color',options.Color,'LineWidth',1.25,'LineStyle','-');
-h.YText = text(ax, x0-0.05*dx, y0+dy/2, ty, 'FontName','Tahoma','HorizontalAlignment','right','Color',options.Color);
-h.XText = text(ax, x0+dx/2, y0-0.05*dy, tx, 'FontName','Tahoma','HorizontalAlignment','center','VerticalAlignment','top','Color',options.Color);
+h.YText = text(ax, x0-0.05*dx, y0+dy/2, ty, 'FontName','Tahoma','HorizontalAlignment','right','Color',options.Color,'FontSize',options.FontSize);
+h.XText = text(ax, x0+dx/2, y0-0.05*dy, tx, 'FontName','Tahoma','HorizontalAlignment','center','VerticalAlignment','top','Color',options.Color,'FontSize',options.FontSize);
 h.XBar.Annotation.LegendInformation.IconDisplayStyle = 'off';
 h.YBar.Annotation.LegendInformation.IconDisplayStyle = 'off';
 end
