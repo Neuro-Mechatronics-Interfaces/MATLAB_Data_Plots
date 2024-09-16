5a84552b8d25ff21d76c27a05624c91cd7ff4404
Max (Lenovo) <max.murphy11@gmail.com>
2024-09-16 14:25:39 -0400

    Adds option to only have XBar or YBar on add_scale_bar

--- add_scale_bar.m
+++ add_scale_bar.m
@@ -11,8 +11,10 @@ arguments
     x1 (1,1) % Ending point of horizontal bar
     y1 (1,1) % Ending point of vertical bar
     options.Color (1,3) double {mustBeInRange(options.Color,0,1)} = [0.65 0.65 0.65];
+    options.XBar (1,1) logical = true;
     options.XUnits = 'sec';
     options.XLabelScaleFactor = 1;
+    options.YBar (1,1) logical = true;
     options.YUnits = 'μV';
     options.YLabelScaleFactor = 1;
     options.FontSize = 12;
@@ -40,10 +42,14 @@ else
 end
 y1 = y0 + dy;
 
-h.XBar = line(ax, [x0, x0], [y0, y1], 'Color',options.Color,'LineWidth',1.25,'LineStyle','-');
-h.YBar = line(ax, [x0, x1], [y0, y0], 'Color',options.Color,'LineWidth',1.25,'LineStyle','-');
-h.YText = text(ax, x0-0.05*dx, y0+dy/2, ty, 'FontName',options.FontName,'HorizontalAlignment','center','VerticalAlignment','bottom','Rotation',90,'Color',options.Color,'FontSize',options.FontSize);
-h.XText = text(ax, x0+dx/2, y0-0.05*dy, tx, 'FontName',options.FontName,'HorizontalAlignment','center','VerticalAlignment','top','Color',options.Color,'FontSize',options.FontSize);
-h.XBar.Annotation.LegendInformation.IconDisplayStyle = 'off';
-h.YBar.Annotation.LegendInformation.IconDisplayStyle = 'off';
+if options.XBar
+    h.XBar = line(ax, [x0, x1], [y0, y0], 'Color',options.Color,'LineWidth',1.25,'LineStyle','-');
+    h.XText = text(ax, x0+dx/2, y0-0.05*dy, tx, 'FontName',options.FontName,'HorizontalAlignment','center','VerticalAlignment','top','Color',options.Color,'FontSize',options.FontSize);
+    h.YBar.Annotation.LegendInformation.IconDisplayStyle = 'off';
+end
+if options.YBar
+    h.YBar = line(ax, [x0, x0], [y0, y1], 'Color',options.Color,'LineWidth',1.25,'LineStyle','-');
+    h.YText = text(ax, x0-0.05*dx, y0+dy/2, ty, 'FontName',options.FontName,'HorizontalAlignment','center','VerticalAlignment','bottom','Rotation',90,'Color',options.Color,'FontSize',options.FontSize);
+    h.YBar.Annotation.LegendInformation.IconDisplayStyle = 'off';
+end
 end
